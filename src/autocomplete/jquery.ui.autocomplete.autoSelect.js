acaab3e2129591bf75eecac4c6b1be52aa44e8fe
Miguel Sousa <msousa@jtmsa.com>
2017-03-14 12:03:04 +0000

    Added support to Bower.
    Upgraded support to newer versions of jquery ui and jquery

--- src/autocomplete/jquery.ui.autocomplete.autoSelect.js
+++ src/autocomplete/jquery.ui.autocomplete.autoSelect.js
@@ -1,29 +1,35 @@
 /*
  * jQuery UI Autocomplete Auto Select Extension
  *
- * Copyright 2010, Scott González (http://scottgonzalez.com)
+ * Copyright 2017, DTWPT (http://dtwpt.com)
  * Dual licensed under the MIT or GPL Version 2 licenses.
  *
- * http://github.com/scottgonzalez/jquery-ui-extensions
+ * https://github.com/dtwpt/jquery-ui.autocomplete.autoselect
  */
-(function( $ ) {
+(function ($) {
 
-$.ui.autocomplete.prototype.options.autoSelect = true;
-$( ".ui-autocomplete-input" ).live( "blur", function( event ) {
-	var autocomplete = $( this ).data( "autocomplete" );
-	if ( !autocomplete.options.autoSelect || autocomplete.selectedItem ) { return; }
+    $.ui.autocomplete.prototype.options.autoSelect = true;
+    +$(document).on('blur', '.ui-autocomplete-input', function (event) {
+        var jQUIver = jQuery.ui.version.split('.');
 
-	var matcher = new RegExp( "^" + $.ui.autocomplete.escapeRegex( $(this).val() ) + "$", "i" );
-	autocomplete.widget().children( ".ui-menu-item" ).each(function() {
-		var item = $( this ).data( "item.autocomplete" );
-		if ( matcher.test( item.label || item.value || item ) ) {
-			autocomplete.selectedItem = item;
-			return false;
-		}
-	});
-	if ( autocomplete.selectedItem ) {
-		autocomplete._trigger( "select", event, { item: autocomplete.selectedItem } );
-	}
-});
+        var autocomplete = ( jQUIver[0] >= 2 || ( jQUIver[0] == 1 && jQUIver[1] >= 10 ) ? $(this).data("uiAutocomplete") : $(this).data("autocomplete") );
 
-}( jQuery ));
+        if (!autocomplete.options.autoSelect || autocomplete.selectedItem) {
+            return;
+        }
+
+        var matcher = new RegExp("^" + $.ui.autocomplete.escapeRegex($(this).val()) + "$", "i");
+        autocomplete.widget().children(".ui-menu-item").each(function () {
+            var item = ( jQUIver[0] >= 2 || ( jQUIver[0] == 1 && jQUIver[1] >= 10 ) ? $(this).data("uiAutocompleteItem") : $(this).data("item.autocomplete") );
+
+            if (matcher.test(item.label || item.value || item)) {
+                autocomplete.selectedItem = item;
+                return false;
+            }
+        });
+        if (autocomplete.selectedItem) {
+            autocomplete._trigger("select", event, {item: autocomplete.selectedItem});
+        }
+    });
+
+}(jQuery));
